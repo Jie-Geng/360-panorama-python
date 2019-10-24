ec83d49a4ff358124e704784db5039bdca2632b7
jie <jiegeng94@outlook.com>
2019-10-24 15:51:19 +0800

    Refresh all the project

--- install_requrirements.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install -y python3
-sudo apt-get install -y python3-pip
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt --user
-sudo apt-get install -y python-tk
